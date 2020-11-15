88badf99ea8e22b55ae328ba5659e1658a64bdc6
Enrique <enenva@gmail.com>
2020-11-15 13:48:40 +0100

    Implemented function getRecentContacts to send 3 contacts, distinct and ordered

--- 2/Contact.kt
+++ 2/Contact.kt
@@ -2,9 +2,8 @@
 /**
  * todo : nothing
  */
-class Contact {
-
-    var id: String = ""
-    var data: String = ""
-    var last_used: Time = Time()
-}
+data class Contact (
+    var id: String,
+    var data: String,
+    var last_used: Time
+)
--- 2/Time.kt
+++ 2/Time.kt
@@ -1,5 +1,5 @@
 
-class Time {
-    val date_iso8601: String = ""
-    val unix: String = ""
-}
+data class Time (
+    val date_iso8601: String,
+    val unix: String
+)
