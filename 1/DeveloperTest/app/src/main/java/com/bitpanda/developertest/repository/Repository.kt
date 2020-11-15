15fd58546fb63468102cbec41e99c968e6b84eaa
Enrique <enenva@gmail.com>
2020-11-15 12:19:50 +0100

    Implemented functions for Repository

--- 1/DeveloperTest/app/src/main/java/com/bitpanda/developertest/repository/Repository.kt
+++ 1/DeveloperTest/app/src/main/java/com/bitpanda/developertest/repository/Repository.kt
@@ -1,18 +1,63 @@
 package com.bitpanda.developertest.repository
 
+import androidx.lifecycle.MutableLiveData
+import com.bitpanda.developertest.model.*
 import com.bitpanda.developertest.remote.DummyWebService
+import javax.inject.Singleton
+import javax.inject.Inject
+import kotlin.reflect.KClass
 
-class Repository(val webservice: DummyWebService) {
+@Singleton
+class Repository @Inject constructor() {
+    private val webservice = DummyWebService()
 
-
-    fun findBySymbol(s: String): IMPLEMENT_ME {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get Currency by Symbol
+    fun findBySymbol(s: String): Currency? {
+        return getCurrencies().find { it.symbol == s }
     }
 
-
-    fun findByName(name: String): IMPLEMENT_ME {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get Currency by Name
+    fun findByName(name: String): Currency? {
+        return getCurrencies().find { it.name == name }
     }
 
+    // Get all currencies
+    private fun getCurrencies(): List<Currency> {
+        return webservice.getCurrencies()
+    }
+
+    // Get Wallets sort by Balance and no show Deleted
+    private fun getWallets(): List<Wallet> {
+        return webservice.getWallets().filter { !it.deleted }.sortedByDescending { it.balance }
+    }
+
+    fun getWalletsFlow(): MutableLiveData<List<Wallet>> {
+        val walletList: MutableLiveData<List<Wallet>> = MutableLiveData()
+        walletList.value = walletSortByType()
+        return walletList
+    }
+
+    // Sort Wallets by Currency type
+    private fun walletSortByType(): List<Wallet>{
+        return getWallets().sortedWith(compareBy {
+            when (it.currency) {
+                is Metal -> 1
+                is Cryptocoin -> 0
+                is Fiat -> -1
+                else -> Integer.MAX_VALUE
+            }
+        })
+    }
+
+    // Get Wallets by Currency Type
+    fun <T: Any> getWalletsByCurrency(currency: KClass<T>?): List<Wallet> {
+        // Filter by Type
+        return when(currency) {
+            Cryptocoin::class -> getWallets().filter { it.currency is Cryptocoin }
+            Fiat::class -> getWallets().filter { it.currency is Fiat }
+            Metal::class -> getWallets().filter { it.currency is Metal }
+            else -> walletSortByType()
+        }
+    }
 
 }
