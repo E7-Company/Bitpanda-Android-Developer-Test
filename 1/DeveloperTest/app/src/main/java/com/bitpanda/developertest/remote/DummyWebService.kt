857168ee66f949fe4f37c1353fc624423355443a
Enrique <enenva@gmail.com>
2020-11-15 12:14:42 +0100

    Fixed dummy data with new data model and implemented functions for dummy web service

--- 1/DeveloperTest/app/src/main/java/com/bitpanda/developertest/remote/DummyWebService.kt
+++ 1/DeveloperTest/app/src/main/java/com/bitpanda/developertest/remote/DummyWebService.kt
@@ -4,32 +4,49 @@ import com.bitpanda.developertest.model.*
 
 class DummyWebService {
 
-    fun getCryptoWallets(): List<CryptocoinWallet> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get wallets by Crytpocoin type
+    fun getCryptoWallets(): List<Wallet> {
+        return DummyData.wallets.filter { it.currency is Cryptocoin }
     }
 
-    fun getMetalWallets(): List<MetalWallet> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get wallets by Metal type
+    fun getMetalWallets(): List<Wallet> {
+        return DummyData.wallets.filter { it.currency is Metal }
     }
 
-    fun getFiatWallets(): List<FiatWallet> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get wallets by Fiat type
+    fun getFiatWallets(): List<Wallet> {
+        return DummyData.wallets.filter { it.currency is Fiat }
     }
 
-    fun getCryptocoins(): List<Cryptocoin> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get currencies of Crytocoin type
+    fun getCryptocoins(): List<Currency> {
+        return DummyData.currencies.filterIsInstance<Cryptocoin>()
     }
 
-    fun getMetals(): List<Metal> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get currencies of Metal type
+    fun getMetals(): List<Currency> {
+        return DummyData.currencies.filterIsInstance<Metal>()
     }
 
-    fun getFiats(): List<Fiat> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get currencies of Fiat type
+    fun getFiats(): List<Currency> {
+        return DummyData.currencies.filterIsInstance<Fiat>()
     }
 
-    fun getCurrencies(): List<IMPLEMENT_ME> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // Get currencies of Asset type
+    fun getAssets(): List<Currency> {
+        return DummyData.currencies.filterIsInstance<Asset>()
+    }
+
+    // Get all currencies
+    fun getCurrencies(): List<Currency> {
+        return DummyData.currencies
+    }
+
+    // Get all wallets
+    fun getWallets(): List<Wallet> {
+        return DummyData.wallets
     }
 
 }
