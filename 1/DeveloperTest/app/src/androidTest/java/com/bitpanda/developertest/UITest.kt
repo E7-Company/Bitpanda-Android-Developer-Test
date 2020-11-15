99705ff6b11f5fa946619d825f57433943e8af18
Enrique <enenva@gmail.com>
2020-11-15 13:06:00 +0100

    Unit and UI tests

---
+++ 1/DeveloperTest/app/src/androidTest/java/com/bitpanda/developertest/UITest.kt
@@ -0,0 +1,87 @@
+package com.bitpanda.developertest
+
+import android.app.Activity
+import android.content.Intent
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.bitpanda.developertest.repository.Repository
+import com.bitpanda.developertest.ui.wallets.WalletActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class UITest {
+
+    @Rule
+    @JvmField
+    val mActivityTestRule = ActivityTestRule(WalletActivity::class.java)
+
+    private lateinit var repository: Repository
+
+    @Before
+    fun init() {
+        mActivityTestRule.activity
+            .supportFragmentManager.beginTransaction()
+
+        repository = Repository()
+
+        mActivityTestRule.restartActivity()
+    }
+
+    @Test
+    fun checkViewsDisplay() {
+        onView(withId(R.id.container))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testWalletsNumberCollection() {
+        onView(withId(R.id.recyclerView))
+            .check(RecyclerViewItemCountAssertion(8))
+    }
+
+    @Test
+    fun testCurrencySymbolCollection() {
+        onView(withId(R.id.recyclerView))
+            .check(matches(hasDescendant(withText("EUR"))))
+    }
+
+    @Test
+    fun testCurrencyBalanceCollection() {
+        onView(withId(R.id.recyclerView))
+            .check(matches(hasDescendant(withText("400.0"))))
+    }
+
+    @Test
+    fun testChangeCurrencyCollection() {
+        onView(withId(R.id.fab))
+            .perform(click())
+
+        onView(withId(R.id.recyclerView))
+            .check(matches(hasDescendant(withText("BTC"))))
+    }
+
+    @Test
+    fun testOpenPriceViewDetail() {
+        onView(withId(R.id.recyclerView))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(3, click()))
+
+        onView(withId(R.id.container))
+            .check(matches(hasDescendant(withText("0.0300€"))))
+    }
+
+    private inline fun <reified T : Activity> ActivityTestRule<T>.restartActivity() {
+        finishActivity()
+        launchActivity(Intent(InstrumentationRegistry.getTargetContext(), T::class.java))
+    }
+
+}
---
+++ 1/DeveloperTest/app/src/test/java/com/bitpanda/developertest/ViewModelTest.kt
@@ -0,0 +1,113 @@
+package com.bitpanda.developertest
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.bitpanda.developertest.model.Cryptocoin
+import com.bitpanda.developertest.model.Fiat
+import com.bitpanda.developertest.model.Metal
+import com.bitpanda.developertest.model.Wallet
+import com.bitpanda.developertest.repository.Repository
+import com.bitpanda.developertest.ui.wallets.WalletViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.instanceOf
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
+class ViewModelTest {
+
+    @Mock
+    private lateinit var context: Application
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+
+    private lateinit var repository: Repository
+    private lateinit var viewModel: WalletViewModel
+
+    private lateinit var onRenderWalletsObserver: Observer<List<Wallet>>
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        `when`(context.applicationContext).thenReturn(context)
+
+        Dispatchers.setMain(testCoroutineDispatcher)
+
+        repository = Repository()
+        viewModel = WalletViewModel(repository)
+
+        onRenderWalletsObserver = mock(Observer::class.java) as Observer<List<Wallet>>
+
+    }
+
+    @Test
+    fun `retrieve all wallets from Repository`() {
+        with(viewModel) {
+            wallets.observeForever(onRenderWalletsObserver)
+        }
+        repository.getWalletsFlow()
+        Assert.assertEquals(viewModel.wallets.value?.size, 8)
+    }
+
+
+    @Test
+    fun `retrieve all wallets from Repository sorted by currency type and balance`() {
+        with(viewModel) {
+            wallets.observeForever(onRenderWalletsObserver)
+        }
+        repository.getWalletsFlow()
+        Assert.assertThat(viewModel.wallets.value?.get(0)?.currency, instanceOf(Fiat::class.java))
+        Assert.assertTrue(viewModel.wallets.value?.get(0)?.balance!! > viewModel.wallets.value?.get(1)?.balance!!)
+    }
+
+    @Test
+    fun `retrieve wallets from Repository filtered by Cryptocoin currency type`() {
+        testCoroutineScope.runBlockingTest {
+            with(viewModel) {
+                wallets.observeForever(onRenderWalletsObserver)
+                changeCurrency()
+            }
+            Assert.assertEquals(viewModel.wallets.value?.size, 3)
+            Assert.assertThat(viewModel.wallets.value?.get(0)?.currency, instanceOf(Cryptocoin::class.java))
+        }
+    }
+
+    @Test
+    fun `retrieve wallets from Repository filtered by Fiat currency type`() {
+        testCoroutineScope.runBlockingTest {
+            with(viewModel) {
+                wallets.observeForever(onRenderWalletsObserver)
+                changeCurrency()
+                changeCurrency()
+            }
+            Assert.assertEquals(viewModel.wallets.value?.size, 2)
+            Assert.assertThat(viewModel.wallets.value?.get(0)?.currency, instanceOf(Fiat::class.java))
+        }
+    }
+
+    @Test
+    fun `retrieve wallets from Repository filtered by Metal currency type`() {
+        testCoroutineScope.runBlockingTest {
+            with(viewModel) {
+                wallets.observeForever(onRenderWalletsObserver)
+                changeCurrency()
+                changeCurrency()
+                changeCurrency()
+            }
+            Assert.assertEquals(viewModel.wallets.value?.size, 3)
+            Assert.assertThat(viewModel.wallets.value?.get(0)?.currency, instanceOf(Metal::class.java))
+        }
+    }
+}
